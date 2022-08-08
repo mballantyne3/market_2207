20d4dc6a98fd8957a80eeb7cd6c255c0a3efca29
Mary Ballantyne <mballantyne777@gmail.com>
2022-08-08 09:35:04 -0600

    create vendor stock methods

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Vendor do
     expect(vendor.name).to eq("Rocky Mountain Fresh")
     expect(vendor.inventory).to eq({})
   end
+
+  it 'can check stock of an item' do
+    expect(vendor.check_stock(item1)).to eq(0)
+  end
+
+  it 'can restock multiple items' do
+    vendor.stock(item1, 30)
+
+    expect(vendor.inventory).to eq({item1 => 30})
+    expect(vendor.check_stock(item1)).to eq(30)
+
+    vendor.stock(item1, 25)
+
+    expect(vendor.check_stock(item1)).to eq(55)
+
+    vendor.stock(item2, 12)
+
+    expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+  end
 end
